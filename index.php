eb19939f63ee1810300455ef8b791f8d6ed004a9
Thibaud BARDIN <thibaud.bardin@gmail.com>
2014-10-07 09:56:02 +0200

    Modify

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
  * @author Thibaud BARDIN (https://github.com/Irvyne).
  * This code is under MIT licence (see https://github.com/Irvyne/license/blob/master/MIT.md)
  */
+
 $link = mysqli_connect(
     'localhost',
     'root',
@@ -10,9 +11,34 @@ $link = mysqli_connect(
     'php_a1_test'
 ) or die('BDD Error');
 
-$result = mysqli_query($link, 'SELECT * FROM article');
+$names = [
+    'dzvezfezfze',
+    'sdfgbjukjuyjkt',
+    'zefzadefzfgzef',
+    'argytjyt',
+];
+/*
+ * CRUD
+ *
+ * Create => INSERT
+ * Read   => SELECT
+ * Update => UPDATE
+ * Delete => DELETE
+ */
 
-while ($row = mysqli_fetch_assoc($result)) {
-    echo '<h1>'.$row['name'].'</h1>';
-    //var_dump($row);
-}
+//$sql = 'INSERT INTO article (id, name) VALUES (NULL, ?)';
+//
+//$prepareStatement = mysqli_prepare($link, $sql);
+//
+//foreach($names as $name) {
+//    mysqli_stmt_bind_param($prepareStatement, 's', $name);
+//    mysqli_stmt_execute($prepareStatement);
+//}
+$name = 'MonNouveauNom';
+$sql = 'UPDATE article SET name="'.mysqli_real_escape_string($link, $name).'" WHERE id=8';
+
+$result = mysqli_query($link, $sql);
+
+var_dump($result);
+
+var_dump(mysqli_error($link));
